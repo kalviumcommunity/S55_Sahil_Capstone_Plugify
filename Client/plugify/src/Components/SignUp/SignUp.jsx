b349d9cd83ebb1e1334e0cf90406569070ddee1a
Sahil Kharatmol <sahilkharatmol09@gmail.com>
2024-04-12 13:36:26 +0530

    Worked on signup/login page(both frontend and backend)

--- Client/plugify/src/Components/SignUp/SignUp.jsx
+++ Client/plugify/src/Components/SignUp/SignUp.jsx
@@ -1,99 +1,213 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import './SignUp.css'; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF, faGoogle, faLinkedinIn,  faXTwitter} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import "./SignUp.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faGoogle,
+  faLinkedinIn,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
 const SignInSignUpForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register: loginRegister,
+    handleSubmit: handleLoginSubmit,
+    formState: { errors: loginErrors },
+  } = useForm();
+  const {
+    register: signupRegister,
+    handleSubmit: handleSignupSubmit,
+    formState: { errors: signupErrors },
+  } = useForm();
+
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
+  const [loginMessage, setLoginMessage] = useState('');
+  const navigate = useNavigate();
 
   const handleSignUpClick = () => {
-    const container = document.querySelector('.signup-container');
-    container.classList.add('sign-up-mode');
+    setIsSignUpMode(true);
   };
 
   const handleSignInClick = () => {
-    const container = document.querySelector('.signup-container');
-    container.classList.remove('sign-up-mode');
+    setIsSignUpMode(false);
   };
 
-  //Created two distinct functions for login and signup forms.
-  const onSubmitSignUp = (data) => {
+  const onSubmitSignUp = async (data) => {
     console.log(data);
+    {
+      const { username, password, email } = data;
+      try {  
+        const response = await axios.post(`https://calf-kings.onrender.com/signup`, { username, password, email });
+        if (response.status === 200) {
+          navigate("/");
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
   };
 
-  const onSubmitSignIn = (data) => {
-    console.log(data);
+  const onSubmitSignIn = async (data) => {
+    const { username, password } = data;
+    try {
+      const response = await axios.post(`https://calf-kings.onrender.com/login`, { username, password });
+      if (response.status === 200) {
+        console.log(username)
+        navigate("/");
+      } else {
+        setLoginMessage('Invalid Credentials');
+      }
+    } catch (err) {
+      console.error(err);
+      setLoginMessage('Invalid Credentials');
+    }
   };
 
   return (
-    <div className="signup-container">
+    <div className={`signup-container ${isSignUpMode ? "sign-up-mode" : ""}`}>
       <div className="forms-container">
         <div className="signin-signup">
-          <form onSubmit={handleSubmit(onSubmitSignIn)} className="sign-in-form">
+          <form
+            onSubmit={handleLoginSubmit(onSubmitSignIn)}
+            className={`sign-in-form ${isSignUpMode ? "hidden" : ""}`}
+          >
             <h2 className="title-signup">Login</h2>
             <div className="input-field">
               <FontAwesomeIcon icon={faUser} className="fas fa-user" />
-              <input type="text" placeholder="Username" {...register('username')} />
+              <input
+                type="text"
+                placeholder="Username"
+                {...loginRegister("username", { required: true })}
+              />
             </div>
+            {loginErrors.username && (
+              <p className="error-message">Username is required</p>
+            )}
             <div className="input-field">
-              <FontAwesomeIcon icon={faLock} className="fas fa-lock"/>
-              <input type="password" placeholder="Password" {...register('password')} />
+              <FontAwesomeIcon icon={faLock} className="fas fa-lock" />
+              <input
+                type="password"
+                placeholder="Password"
+                {...loginRegister("password", {
+                  required: true,
+                })}
+              />
             </div>
+            {loginErrors.password && (
+              <p className="error-message">{loginErrors.password.message}</p>
+            )}
             <input type="submit" value="Login" className="signup-btn solid" />
             <p className="social-text">Or Login with social platforms</p>
             <div className="social-icons flex-centre">
-                <a href="https://www.instagram.com/sahil_k17/" className="facebook bg-color">
-                  <FontAwesomeIcon icon={faFacebookF} className="icon" />
-                </a>
-                <a href="https://www.instagram.com/sahil_k17/" className="twitter bg-color">
-                  <FontAwesomeIcon icon={faXTwitter} className="icon " />
-                </a>
-                <a href="https://www.instagram.com/sahil_k17/" className="google bg-color">
-                  <FontAwesomeIcon icon={faGoogle} className="icon" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/sahil-kharatmol-229912273/"
-                  className="linkedin bg-color"
-                >
-                  <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
-                </a>
-              </div>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="facebook bg-color"
+              >
+                <FontAwesomeIcon icon={faFacebookF} className="icon" />
+              </a>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="twitter bg-color"
+              >
+                <FontAwesomeIcon icon={faXTwitter} className="icon " />
+              </a>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="google bg-color"
+              >
+                <FontAwesomeIcon icon={faGoogle} className="icon" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sahil-kharatmol-229912273/"
+                className="linkedin bg-color"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+              </a>
+            </div>
           </form>
-          <form onSubmit={handleSubmit(onSubmitSignUp)} className="sign-up-form">
+          <form
+            onSubmit={handleSignupSubmit(onSubmitSignUp)}
+            className={`sign-up-form ${isSignUpMode ? "" : "hidden"}`}
+          >
             <h2 className="title-signup">Sign up</h2>
             <div className="input-field">
               <FontAwesomeIcon icon={faUser} className="fas fa-user" />
-              <input type="text" placeholder="Username" {...register('username')} />
+              <input
+                type="text"
+                placeholder="Username"
+                {...signupRegister("username", { required: true })}
+              />
             </div>
+            {signupErrors.username && (
+              <p className="error-message">Username is required</p>
+            )}
             <div className="input-field">
               <FontAwesomeIcon icon={faEnvelope} className="fas fa-envelope" />
-              <input type="email" placeholder="Email" {...register('email')} />
+              <input
+                type="email"
+                placeholder="Email"
+                {...signupRegister("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+              />
             </div>
+            {signupErrors.email && (
+              <p className="error-message">{signupErrors.email.message}</p>
+            )}
             <div className="input-field">
               <FontAwesomeIcon icon={faLock} className="fas fa-lock" />
-              <input type="password" placeholder="Password" {...register('password')} />
+              <input
+                type="password"
+                placeholder="Password"
+                {...signupRegister("password", {
+                  required: true,
+                  minLength: {
+                    value: 6,
+                    message: "Password should be more than 5 characters",
+                  },
+                })}
+              />
             </div>
+            {signupErrors.password && (
+              <p className="error-message">{signupErrors.password.message}</p>
+            )}
             <input type="submit" value="Sign up" className="signup-btn" />
             <p className="social-text">Or Sign up with social platforms</p>
             <div className="social-icons flex-centre">
-                <a href="https://www.instagram.com/sahil_k17/" className="facebook bg-color">
-                  <FontAwesomeIcon icon={faFacebookF} className="icon" />
-                </a>
-                <a href="https://www.instagram.com/sahil_k17/" className="twitter bg-color">
-                  <FontAwesomeIcon icon={faXTwitter} className="icon " />
-                </a>
-                <a href="https://www.instagram.com/sahil_k17/" className="google bg-color">
-                  <FontAwesomeIcon icon={faGoogle} className="icon" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/sahil-kharatmol-229912273/"
-                  className="linkedin bg-color"
-                >
-                  <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
-                </a>
-              </div>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="facebook bg-color"
+              >
+                <FontAwesomeIcon icon={faFacebookF} className="icon" />
+              </a>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="twitter bg-color"
+              >
+                <FontAwesomeIcon icon={faXTwitter} className="icon " />
+              </a>
+              <a
+                href="https://www.instagram.com/sahil_k17/"
+                className="google bg-color"
+              >
+                <FontAwesomeIcon icon={faGoogle} className="icon" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sahil-kharatmol-229912273/"
+                className="linkedin bg-color"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+              </a>
+            </div>
           </form>
         </div>
       </div>
@@ -102,18 +216,36 @@ const SignInSignUpForm = () => {
         <div className="panel left-panel">
           <div className="content">
             <h3>New to our community ?</h3>
-            <p>Discover a world of possibilities! Join us and explore a vibrant community where ideas flourish and connections thrive.</p>
-            <button className="abv-btn transparent" onClick={handleSignUpClick}>Sign up</button>
+            <p>
+              Discover a world of possibilities! Join us and explore a vibrant
+              community where ideas flourish and connections thrive.
+            </p>
+            <button className="abv-btn transparent" onClick={handleSignUpClick}>
+              Sign up
+            </button>
           </div>
-          <img src="https://i.ibb.co/6HXL6q1/Privacy-policy-rafiki.png" className="image" alt="" />
+          <img
+            src="https://i.ibb.co/6HXL6q1/Privacy-policy-rafiki.png"
+            className="image"
+            alt=""
+          />
         </div>
         <div className="panel right-panel">
           <div className="content">
             <h3>One of Our Valued Members</h3>
-            <p>Thank you for being part of our community. Your presence enriches our shared experiences. Let's continue this journey together!</p>
-            <button className="abv-btn transparent" onClick={handleSignInClick}>Login</button>
+            <p>
+              Thank you for being part of our community. Your presence enriches
+              our shared experiences. Let's continue this journey together!
+            </p>
+            <button className="abv-btn transparent" onClick={handleSignInClick}>
+              Login
+            </button>
           </div>
-          <img src="https://i.ibb.co/nP8H853/Mobile-login-rafiki.png" className="image" alt="" />
+          <img
+            src="https://i.ibb.co/nP8H853/Mobile-login-rafiki.png"
+            className="image"
+            alt=""
+          />
         </div>
       </div>
     </div>
--- Server/routes.js
+++ Server/routes.js
@@ -1,27 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { getConnectionStatus } = require('./db');
-const { userModel } = require('./schema');
-app.use(express.json())
+const { getConnectionStatus } = require("./db");
+const { userModel } = require("./schema");
+const { adminModel } = require("./adminSchema.js");
+app.use(express.json());
 
 // GET request to get connection status
-app.get('/', async (req, res) => {
-    const connectionStatus = await getConnectionStatus();
-    res.send(connectionStatus);
+app.get("/", async (req, res) => {
+  const connectionStatus = await getConnectionStatus();
+  res.send(connectionStatus);
 });
 
-app.get('/data', async (req, res) => {
-    try {
-        const users = await userModel.find();
-        res.json(users);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+app.get("/data", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/adminsignup", async (req, res) => {
+  try {
+    const user = await adminModel.create({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    });
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+  }
+});
+app.post("/adminlogin", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await adminModel.findOne({ username, password });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username / password" });
     }
+
+    res.status(200).json({ user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.get('/hello', function(req, res){
-  res.send('Hello')
-})
+app.post("/adminlogout", (req, res) => {
+  res.clearCookie("username");
+  res.clearCookie("password");
+  res.clearCookie("email");
+
+  res.status(200).json({ message: "Logout succesful" });
+});
+
+app.get("/hello", function (req, res) {
+  res.send("Hello");
+});
 
 module.exports = app;
